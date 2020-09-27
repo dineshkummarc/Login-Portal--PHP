6785ef491135fb3e2dab69482e1c070bece59bbf
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-27 13:12:18 +0530

    update

--- session.php
+++ session.php
@@ -3,10 +3,10 @@
    session_start();
    
    $user_check = $_SESSION['login_user'];
-   $ses_sql = mysql_query("SELECT username FROM users where username = '$user_check'");
+   $ses_sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT username FROM users where username = '$user_check'");
    
    
-   $row = mysql_fetch_array($ses_sql);
+   $row = mysqli_fetch_array($ses_sql);
    
    $login_session = $row['username'];
    
