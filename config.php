c5ec7911e936a6413982966458b3d0b3eee12dd1
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-27 13:11:28 +0530

    update

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
    define('DB_HOST', 'localhost');
-define('DB_NAME', 'final');
+define('DB_NAME', 'FinalIssues');
 define('DB_USER','root');
-define('DB_PASSWORD','rootpass');
+define('DB_PASSWORD','');
 
-$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+$con=($GLOBALS["___mysqli_ston"] = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD)) or die("Failed to connect to mysql: " . mysqli_error($GLOBALS["___mysqli_ston"]));
+$db=mysqli_select_db($con, constant('DB_NAME')) or die("Failed to connect to mysql: " . mysqli_error($GLOBALS["___mysqli_ston"]));
 
 ?>
